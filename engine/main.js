1146cb0ff05ce71af723a38a6d0dc3ae20e53411
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-25 19:22:36 -0300

    binance initialization

--- engine/main.js
+++ engine/main.js
@@ -6,7 +6,7 @@ const binance = nba().options({
   APIKEY: process.env.BINANCE_KEY,
   APISECRET: process.env.BINANCE_SECRET,
   useServerTime: true,
-  test: true
+  test: process.env.BINANCE_SANDBOX
 });
 
 // Hardcoded alghoritm
