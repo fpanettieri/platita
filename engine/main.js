edebff806473a064450400f9ae0fda005b233384
Fabio Panettieri <fpanettieri@gmail.com>
2018-09-25 21:26:47 -0300

    klines approach

--- engine/main.js
+++ engine/main.js
@@ -13,6 +13,12 @@ const binance = nba().options({
 
 
 // Get the needed candles to calculate
+binance.candlesticks("POEBTC", "1D", (error, ticks, symbol) => {
+  console.log("candlesticks()", ticks);
+  // let last_tick = ticks[ticks.length - 1];
+  // let [time, open, high, low, close, volume, closeTime, assetVolume, trades, buyBaseVolume, buyAssetVolume, ignored] = last_tick;
+  console.log(symbol + " last close: " + close);
+}, {limit: 20});
 
 
 console.log('done');
