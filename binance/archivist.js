2a905edbfdf76b84441f2b0b37aa37be00c4c6c6
Fabio Panettieri <fpanettieri@gmail.com>
2018-10-13 21:15:04 -0300

    socket wip

--- binance/archivist.js
+++ binance/archivist.js
@@ -7,7 +7,7 @@ const utils   = require('../lib/utils');
 const Logger = require('../lib/logger');
 const logger = new Logger('[binance/archivist]');
 
-// Holder for Mongo instance
+// Holder for Mongo Database instance
 let db = null;
 
 function cliHero ()
@@ -28,22 +28,27 @@ function cliHelp ()
   process.exit();
 }
 
-function dispatchMsg (msg)
+function dispatchMsg (msg, socket)
 {
   logger.log('dispatching msg', msg);
   switch (msg[0]) {
     case "DownloadFirstCandle": {
-      downloadFirstCandle(msg[1], msg[2]);
+      downloadFirstCandle(msg[1], msg[2], socket);
     } break;
 
     case "DownloadHistory": {
-      downloadHistory(msg[1], msg[2]);
+      downloadHistory(msg[1], msg[2], socket);
     } break;
   }
 }
 
-function downloadFirstCandle (symbol, interval)
+function downloadFirstCandle (symbol, interval, socket)
 {
+  socket.write('<watch out>');
+  return
+  // check if the symbol is already initialized in mongo
+
+
   binance.candlesticks(symbol, interval, (error, ticks, _symbol) => {
     if (error) {
       backbone.emit('error', 'Symbol initialization failed: ', symbol, interval);
@@ -68,7 +73,7 @@ function downloadFirstCandle (symbol, interval)
     // Watcher: watch the market in real time, store the candles in the database
     // Analyst: After each candle arrives
 
-    // backbone.emit('SymbolInitialized', symbol, interval);
+    // socket.write(`SymbolInitialized ${symbol} ${interval}`);
   }, {
     limit: 2,
     startTime: 0
