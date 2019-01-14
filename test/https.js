8d4e69f7e3331ec4a1e6144c1cea5c79d6b7dd18
Fabio Panettieri <fpanettieri@gmail.com>
2019-01-14 03:04:26 -0300

    bitmex platita

---
+++ test/https.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const https = require('../lib/https');
+const logger = require('../lib/logger');
+
+const log = new logger(`[test/https]`);
+
+(async () => {
+  try {
+    // const rsp = await https.get('https://www.google.com');
+    const rsp = await https.get('https://testnet.bitmex.com/api/v1/trade/bucketed?binSize=1m&symbol=XBTUSD&count=3&startTime=0&partial=false');
+
+    log.log('status', rsp.status);
+    log.log('headers', rsp.headers);
+    log.log('body', rsp.body);
+  } catch(err) {
+    log.error(err);
+  }
+})();
