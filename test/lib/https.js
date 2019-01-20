d8d6606598b15c0771a7dcdc71fdf885cf9976a1
Fabio Panettieri <fpanettieri@gmail.com>
2019-01-20 12:14:40 -0300

    https test

--- test/lib/https.js
+++ test/lib/https.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const https = require('../lib/https');
-const logger = require('../lib/logger');
+const https  = require('../../lib/https');
 
+const logger = require('../../lib/logger');
 const log = new logger(`[test/https]`);
 
 (async () => {
   try {
-    // const rsp = await https.get('https://www.google.com');
     const rsp = await https.get('https://testnet.bitmex.com/api/v1/trade/bucketed?binSize=1m&symbol=XBTUSD&count=3&startTime=0&partial=false');
 
     log.log('status', rsp.status);
---
+++ test/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const assert   = require('assert');
+const net      = require('net');
+
+const Logger = new require('../lib/logger');
+const logger = new Logger('[test]');
