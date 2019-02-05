bb30f8847f0e052397e71f4d14e23fa0a4b5a9fe
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-05 03:38:11 -0300

    watcher cleanup

--- binance/watcher.js
+++ binance/watcher.js
@@ -19,22 +19,20 @@ function watchSymbol (symbol, interval, socket)
 {
   try {
     const id = `binance_${symbol}_${interval}`.toLowerCase();
+    ms.logger.info(`watching ${id}`);
 
     const raw_col = ms.db.collection(`${id}_raw`);
     const ohlc_col = ms.db.collection(`${id}_ohlc`);
 
-    ms.logger.info(`watching ${id}`);
-
     Binance.websockets.candlesticks(symbol, interval, (candlestick) => {
-      const c = candlestick.k;
-      const e = c.x ? 'CandleClosed' : 'CandleUpdated';
-      socket.send({e: e, candle: candlestick.k});
+      const raw = candlestick.k;
+      const ohlc = (o) => binance.toOhlc(raw);
+      const ev = raw.x ? 'CandleClosed' : 'CandleUpdated';
+      socket.send({e: ev, s: symbol, i: interval, raw: raw, ohlc: ohlc});
 
       // Store closed candles
-      if (!c.x) { return }
-      raw_col.replaceOne({t: c.t}, c, {upsert: true});
-
-      const ohlc = (o) => binance.toOhlc(c);
+      if (!raw.x) { return }
+      raw_col.replaceOne({t: raw.t}, raw, {upsert: true});
       ohlc_col.replaceOne({t: ohlc.t}, ohlc, {upsert: true});
     });
 
